399c8ef838d0864a9527e5baac659534d7cade33
inka17 <innksang1007@gmail.com>
2025-12-30 04:53:29 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -14,44 +14,79 @@ void initialize(STACK* s, size_t mem_size)
 	s->stack_pointer = NULL;
 	s->stack_memory = NULL;
 	s->end = NULL;
+	
+	if (mem_size < sizeof(int)) return;
+
+	s->stack_memory = (int*)malloc(mem_size);
+	if (!s->stack_memory) return;
+
+	s->stack_pointer = s->stack_memory;
+	s->end = s->stack_memory + (mem_size / sizeof(int));
 }
 
 
 // 確保したメモリを解放する
 void finalize(STACK* s)
 {
-	// ToDo: Initializeで確保したメモリを解放しよう
+	if (!s) return;
+
+	free(s->stack_memory);
+
+	s->stack_pointer = NULL;
+	s->stack_memory = NULL;
+	s->end = NULL;
 }
 
 
 // valの値をスタックに積む。実行の成否を返す
 bool push(STACK* s, int val)
 {
-	// ToDo: valの値をスタックに保存しよう
-	return false;
+	if (!s || !s->stack_memory) return false;
+	if (s->stack_pointer >= s->end) return false;
+
+	*s->stack_pointer++ = val;
+	return true;
 }
 
 
 // addrから始まるnum個の整数をスタックに積む。実行の成否を返す
 bool push_array(STACK* s, int* addr, int num)
 {
-	// ToDo: addrからはじまるnum個の整数をスタックに保存しよう
-	return false;
+	if (!s || !s->stack_memory || !addr || num <= 0) return false;
+	if (s->end - s->stack_pointer < num) return false;
+
+	for (int i = num; i > 0; i--)
+	{
+		*(s->stack_pointer) = addr[i - 1];
+		s->stack_pointer++;
+	}
+
+	return true;
 }
 
 // スタックから一つの要素を取り出す
 int pop(STACK* s)
 {
-	// ToDo: スタックの最上位の値を取り出して返そう
-	// 不具合時は0を返す
-	return 0;
+	if (!s || !s->stack_memory) return 0;
+	if (s->stack_pointer <= s->stack_memory) return 0;
+
+	s->stack_pointer = s->stack_pointer - 1;
+	return *(s->stack_pointer);
 }
 
 // addrにスタックからnumの要素を取り出す。取り出せた個数を返す
 int pop_array(STACK* s, int* addr, int num)
 {
-	// ToDo: スタックからnum個の値を取り出してaddrから始まるメモリに保存しよう
-	// スタックにnum個の要素がたまっていなかったら、積まれている要素を返して、
-	// 積んだ要素数を返り値として返そう
-	return 0;
+	if (!s || !s->stack_memory || !addr || num <= 0) return 0;
+
+	int count = 0;
+
+	while (count < num && s->stack_pointer > s->stack_memory)
+	{
+		s->stack_pointer--;
+		addr[count] = *(s->stack_pointer);
+		count++;
+	}
+
+	return count;
 }
